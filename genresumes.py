47e2ebf2a39948f3aee4e4c8369459fa0f33bbdf
David Hu <davidhu91@gmail.com>
2012-04-26 02:19:14 -0400

    add command line args and help msg to genresumes.py using optparse

--- genresumes.py
+++ genresumes.py
@@ -7,14 +7,13 @@ templating engine
 
 from Cheetah.Template import Template
 from datetime import datetime
-import sys
-import yaml
-import escape
+from optparse import OptionParser
+import escapes
 import functools
 import os
+import sys
+import yaml
 
-# TODO: rename template.txt to template.txt.tmpl
-# TODO: better directory structure
 
 def escape_leaves(escape_func, contents):
     """Escapes all leaves in the contents dict with escape_func"""
@@ -26,18 +25,41 @@ def escape_leaves(escape_func, contents):
     else:
         return escape_func(str(contents))
 
+def get_cmd_line_args():
+    parser = OptionParser(usage="%prog [options] <extension>",
+        description="Fills in Cheetah templates from YAML source to " +
+                    "generate resume of the given <extension> format.")
+
+    parser.add_option("-o", "--output_dir",
+        default='output',
+        help="Output directory of generated files. Defaults to output/")
+
+    parser.add_option("-s", "--source",
+        default='resume.yaml',
+        help="Filename of the resume YAML source. Defaults to resume.yaml")
+
+    parser.add_option("-t", "--template",
+        default='templates/resume.%s.tmpl',
+        help="Filename of the output format template. Defaults to templates/resume.<extension>.tmpl")
+
+    options, extra_args = parser.parse_args()
+    if len(extra_args) < 1:
+        parser.print_help()
+        exit()
+
+    return options, extra_args[0]
+
 if __name__ == '__main__':
-    extension = sys.argv[1]
+    options, extension = get_cmd_line_args()
 
-    # TODO: cmd line argument
-    contents = yaml.load(open('resume.yaml', 'r').read())
+    contents = yaml.load(open(options.source, 'r').read())
 
-    escape_func = getattr(escape, 'escape_' + extension, lambda x: x)
+    escape_func = getattr(escapes, 'escape_' + extension, lambda x: x)
     escaped_contents = escape_leaves(escape_func, contents)
-    template = Template(file='templates/resume.%s.tmpl' % extension,
+    template = Template(file=options.template % extension,
             searchList=[escaped_contents])
 
-    output_dir = 'output'
+    output_dir = options.output_dir
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     open('%s/resume.%s' % (output_dir, extension), 'w').write(str(template))
